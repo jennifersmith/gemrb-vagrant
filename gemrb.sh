ad1d04d36e27a2cab6c4f39aad214b9609d4d3de
Jennifer Smith <jennifer@home>
2014-04-24 12:35:35 +1000

    Now we have the minimal feature set running - had to adapt the one packaged with gemrb as it seems to think it is installed in /usr/share when actually it gets installed into /usr/share/games

---
+++ gemrb.sh
@@ -0,0 +1,5 @@
+echo "*****gemrb*****"
+sudo apt-get install -y software-properties-common python-software-properties
+sudo add-apt-repository -y ppa:sao/ppa 
+sudo apt-get update 
+sudo apt-get install -y gemrb 
